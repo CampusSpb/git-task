935b97cdff63493de2c57ce0b258d3eae92fbd21
Pavel Egorov <xoposhiy@mail.ru>
2018-11-14 01:03:33 +0500

    handle incorrect user input

--- Calculator.cs
+++ Calculator.cs
@@ -6,6 +6,13 @@ namespace Kontur.Courses.Git
 	public class Calculator
 	{
 		private Maybe<double> lastResult = 0;
+		private Maybe<double> TryParseDouble(string s)
+		{
+			double v;
+			if (double.TryParse(s, out v))
+				return v;
+			return Maybe<double>.FromError("Not a number '{0}'", s);
+		}
 
 		public Maybe<double> Calculate(string[] args)
 		{
@@ -20,12 +27,17 @@ namespace Kontur.Courses.Git
 			}
 			if (args.Length == 3)
 			{
-				var v1 = double.Parse(args[0]);
-				var v2 = double.Parse(args[2]);
-				return lastResult = Execute(args[1], v1, v2);
+				var v1 = TryParseDouble(args[0]);
+				var v2 = TryParseDouble(args[2]);
+				if (!v1.HasValue || !v2.HasValue) return v1;
+				var result = Execute(args[1], v1.Value, v2.Value);
+				if (result.HasValue)
+					lastResult = result;
+				return result;
 			}
 			return Maybe<double>.FromError("Error input");
 		}
+
 
 		private Maybe<double> Execute(string op, double v1, double v2)
 		{
